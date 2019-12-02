ae0ed19810b4092daaba70bef3097891a0083ad8
liqingyong02 <liqingyong02@meituan.com>
2019-12-03 00:41:33 +0800

    udfs

---
+++ src/main/scala/com/meituan/meishi/data/lqy/flink/udfs/AggregateFunctionExample.scala
@@ -0,0 +1,124 @@
+package com.meituan.meishi.data.lqy.flink.udfs
+
+import java.lang.{Integer => JInteger}
+
+import org.apache.flink.api.common.typeinfo.TypeInformation
+import org.apache.flink.api.java.tuple.{Tuple, Tuple1 => JTuple1}
+import org.apache.flink.api.java.typeutils.{RowTypeInfo, TupleTypeInfo}
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.table.api.Types
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.functions.AggregateFunction
+import org.apache.flink.table.planner
+import org.apache.flink.table.planner.JInt
+import org.apache.flink.types.Row
+
+import scala.collection.mutable
+
+/**
+ * 多行转一行， 聚合函数 agg
+ */
+object AggregateFunctionExample extends App {
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val tEnv = StreamTableEnvironment.create(env)
+  env.setParallelism(1)
+  env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
+  val weiAgg = new WeightedAgg
+  tEnv.registerFunction("wAvg", weiAgg)
+
+  val data = new mutable.MutableList[(JInteger, JInteger, String)]
+  data.+=((1, 1, "A"))
+  data.+=((2, 2, "B"))
+  data.+=((2, 2, "B"))
+  data.+=((4, 3, "C"))
+  data.+=((5, 3, "C"))
+  data.+=((4, 3, "C"))
+  data.+=((7, 3, "B"))
+  data.+=((1, 4, "A"))
+  data.+=((9, 4, "D"))
+  data.+=((4, 1, "A"))
+  data.+=((3, 2, "B"))
+
+  val table = env.fromCollection(data)
+    .toTable(tEnv, 'a, 'b, 'c)
+
+  val aggTable = table
+    .groupBy('c)
+    .select('c,
+      'a.count.distinct,
+      'a.sum.distinct,
+      weiAgg.distinct('a, 'b),
+      weiAgg.distinct('b, 'a),
+      weiAgg('a, 'b)
+    )
+
+  aggTable.toRetractStream[Row].filter(_._1).map(_._2).print("agg")
+
+  env.execute("Table Agg Func")
+}
+
+class WeightedAvgAcc {
+  var sum = 0
+  var count = 0
+}
+
+class WeightedAgg extends AggregateFunction[JInteger, WeightedAvgAcc] {
+  override def createAccumulator(): WeightedAvgAcc = {
+    new WeightedAvgAcc
+  }
+
+  override def getValue(accumulator: WeightedAvgAcc): JInteger = {
+    if (accumulator.count == 0) {
+      null
+    } else {
+      accumulator.sum / accumulator.count
+    }
+  }
+
+  def accumulate(acc: WeightedAvgAcc, iValue: JInteger, iWeight: JInteger): Unit = {
+    acc.sum += iValue * iWeight
+    acc.count += iWeight
+  }
+
+  def retract(acc: WeightedAvgAcc, iValue: JInteger, iWeight: JInteger): Unit = {
+    acc.sum -= iValue * iWeight
+    acc.count -= iWeight
+  }
+
+  def merge(acc: WeightedAvgAcc, it: java.lang.Iterable[WeightedAvgAcc]): Unit = {
+    val iter = it.iterator()
+    while (iter.hasNext) {
+      val a = iter.next()
+      acc.count += a.count
+      acc.sum += a.sum
+    }
+  }
+
+  def resetAccumulator(acc: WeightedAvgAcc): Unit = {
+    acc.count = 0
+    acc.sum = 0
+  }
+
+//  override def getAccumulatorType: TypeInformation[WeightedAvgAcc] = {
+//    new RowTypeInfo[WeightedAvgAcc](classOf[WeightedAvgAcc],
+//      Types.LONG, Types.INT)
+//  }
+//
+//  override def getResultType: TypeInformation[JLong] = {
+//    Types.LONG()
+//  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/scala/com/meituan/meishi/data/lqy/flink/udfs/ScalarFunctionExample.scala
@@ -0,0 +1,38 @@
+package com.meituan.meishi.data.lqy.flink.udfs
+
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.functions.ScalarFunction
+import org.apache.flink.types.Row
+
+/**
+ * 单行输入
+ * 单行输出
+ */
+object ScalarFunctionExample extends App {
+  val env = ExecutionEnvironment.getExecutionEnvironment
+  val tableEnv = BatchTableEnvironment.create(env)
+
+  val hash: HashCode = new HashCode(10)
+  tableEnv.registerFunction("hashCode", hash)
+
+  val table = tableEnv.fromDataSet(env.fromElements(("a", "lqy"), ("b", "cde")), 'key, 'vals)
+  tableEnv.registerTable("myTable", table)
+
+  table.select('key, hash('vals) as 'hash_code)
+      .toDataSet[Row].print("table")
+
+  tableEnv.sqlQuery(
+    """
+      |SELECT key, hashCode(vals) AS hash_code
+      |FROM myTable
+      |""".stripMargin).toDataSet[Row].print("sql")
+
+  env.execute("scala r func")
+}
+
+class HashCode(factor: Int) extends ScalarFunction {
+  def eval(s: String): Int = {
+    s.hashCode() * factor
+  }
+}
